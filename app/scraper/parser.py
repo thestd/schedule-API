323058b635585aff619283769c7d4a08c90e3a2b
Ivan Nyzhnyk <thisismyperfectusername@gmail.com>
2019-06-06 16:41:02 +0300

    fixed naming

--- app/scraper/parser.py
+++ app/scraper/parser.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 
 
-def parse_lesson_row(lesson):
-    info = lesson.find_all('td')
+def parse_raw_lesson(raw_lesson):
+    info = raw_lesson.find_all('td')
     lesson_number = info[0].contents[0]
 
     time_bounds = "".join(str(item) for item in info[1].contents).replace('<br/>', ' - ')
@@ -18,34 +18,42 @@ def parse_lesson_row(lesson):
     return parsed_lesson
 
 
-def parse_lessons(lessons):
+def parse_raw_lessons(raw_lessons):
 
-    parsed_lessons = []
+    parsed_lessons_list = []
 
-    for lesson_row in lessons:
-        parsed_lesson = parse_lesson_row(lesson_row)
-        parsed_lessons.append(parsed_lesson)
+    for raw_lesson in raw_lessons:
+        parsed_lesson = parse_raw_lesson(raw_lesson)
+        parsed_lessons_list.append(parsed_lesson)
 
-    return parsed_lessons
+    return parsed_lessons_list
 
 
-def parse_fragments(fragments):
+def parse_raw_day(fragment):
+
+    header = fragment.find_next('h4').contents
+    day_schedule = fragment.find_next('table')
+    raw_lessons = day_schedule.find_all('tr')
+
+    date = header[0]
+    day = header[1].contents[0]
+    parsed_lessons_list = parse_raw_lessons(raw_lessons)
+
+    return date, day, parsed_lessons_list
+
+
+def parse_raw_days(raw_days_list):
 
     schedule = []
 
-    for fragment in fragments:
-        header = fragment.find_next('h4').contents
-        date = header[0]
-        day = header[1].contents[0]
+    for raw_day in raw_days_list:
 
-        day_schedule = fragment.find_next('table')
-        lessons = day_schedule.find_all('tr')
-        parsed_lessons = parse_lessons(lessons)
+        date, day, parsed_lessons_list = parse_raw_day(raw_day)
 
         schedule_item = {
             'date': date,
             'day': day,
-            'items': parsed_lessons
+            'items': parsed_lessons_list
 
         }
         schedule.append(schedule_item)
@@ -54,28 +62,38 @@ def parse_fragments(fragments):
 
 
 def parse_schedule(body):
+
     soup = BeautifulSoup(body, 'lxml')
 
-    fragments_with_days = soup.find_all('div', class_='col-md-6')[1:]
-    schedule = parse_fragments(fragments_with_days)
+    raw_days_list = soup.find_all('div', class_='col-md-6')[1:]
+    schedule = parse_raw_days(raw_days_list)
 
     return schedule
+
+
+def parse_options_list(options):
+
+    faculties_list = []
+
+    for option in options:
+
+        faculties_list.append({
+            'name': option.text,
+            'code': int(option['value'])
+        })
+
+    return faculties_list
 
 
 def parse_faculties(body):
 
     soup = BeautifulSoup(body, 'lxml')
-    faculties = []
 
     form_field = soup.find('select', id='faculty')
     options = form_field.find_all('option')[1:]
 
-    for option in options:
-        faculties.append({
-            'name': option.text,
-            'code': int(option['value'])
-        })
+    faculties_list = parse_options_list(options)
 
-    return faculties
+    return faculties_list
 
 
